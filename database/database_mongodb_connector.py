7e7467f23592a864b059dc9f8495358a99ba110b
Vegas30 <i_valuiskov@mail.ru>
2025-07-09 21:34:27 +0400

    Initial project structure

---
+++ database/database_mongodb_connector.py
@@ -0,0 +1,5 @@
+# Подключение к MongoDB
+
+def get_mongo_connection():
+    # TODO: Реализовать подключение к базе MongoDB
+    pass
---
+++ database/database_postgres_connector.py
@@ -0,0 +1,5 @@
+# Подключение к PostgreSQL
+
+def get_pg_connection():
+    # TODO: Реализовать подключение к базе PostgreSQL
+    pass
