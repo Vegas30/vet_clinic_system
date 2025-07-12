dcdc2f8774bd99652763087040ec167e26271b83
Vegas30 <i_valuiskov@mail.ru>
2025-07-12 17:09:01 +0400

    реализовано подключение к базам данных PostreSQL и MongoDB

--- database/database_mongodb_connector.py
+++ database/database_mongodb_connector.py
@@ -1,5 +1,14 @@
 # Подключение к MongoDB
+from pymongo import MongoClient
 
 def get_mongo_connection():
     # TODO: Реализовать подключение к базе MongoDB
-    pass
+    """Подключение к MongoDB"""
+    try:
+        # Подключение к MongoDB
+        client = MongoClient("mongodb://localhost:27017/")
+        db = client["vet_clinic_mDB"]
+        return db
+    except Exception as e:
+        print(f"Ошибка подключения к MongoDB: {e}")
+        return None
--- database/database_postgres_connector.py
+++ database/database_postgres_connector.py
@@ -1,5 +1,21 @@
 # Подключение к PostgreSQL
+import psycopg2
 
 def get_pg_connection():
     # TODO: Реализовать подключение к базе PostgreSQL
-    pass
+    """Подключение к PostgreSQL"""
+    try:
+        # Подключение к PostgreSQL
+        # Измените пароль подключения согласно вашим настройкам
+        connection = psycopg2.connect(
+            host="localhost",
+            port="5432",
+            database="vet_clinica_pSQL",
+            user="postgres",
+            password="7773"
+        )
+        return connection
+    except Exception as e:
+        print(f"Ошибка подключения к PostgreSQL: {e}")
+        return None    
+
