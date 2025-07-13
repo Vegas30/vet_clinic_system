008f0e5ffd5d96264b26c4943d9f6ccf2f13ba07
Vegas30 <i_valuiskov@mail.ru>
2025-07-13 17:53:27 +0400

    изменены коннекторы к базам данных, теперь это Классы (и названия другие)

--- database/database_mongodb_connector.py
+++ database/database_mongodb_connector.py
@@ -1,14 +1,32 @@
-# Подключение к MongoDB
+import os
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
-def get_mongo_connection():
-    # TODO: Реализовать подключение к базе MongoDB
-    """Подключение к MongoDB"""
-    try:
-        # Подключение к MongoDB
-        client = MongoClient("mongodb://localhost:27017/")
-        db = client["vet_clinic_mDB"]
-        return db
-    except Exception as e:
-        print(f"Ошибка подключения к MongoDB: {e}")
-        return None
+class MongoDBConnector:
+    def __init__(self):
+        load_dotenv()
+        self.mongo_url = os.getenv("MONGO_URL")
+        self.client = None
+        self.db = None
+
+    def connect(self):
+        if self.client is None or not self.client.admin.command('ping'):
+            try:
+                self.client = MongoClient(self.mongo_url)
+                self.db = self.client.get_database()
+                print("Успешное подключение к MongoDB")
+            except Exception as e:
+                print(f"Ошибка подключения к MongoDB: {e}")
+                self.client = None
+                self.db = None
+        return self.db
+
+    def disconnect(self):
+        if self.client:
+            self.client.close()
+            print("Отключение от MongoDB")
+        self.client = None
+        self.db = None
+
+    def get_database(self):
+        return self.db
